2f8807dc6de7205be68a1a5362b3bfe7d747b874
零弍 <reiji_content@yahoo.co.jp>
2022-03-26 22:34:22 +0900

    feat: set card

--- game.js
+++ game.js
@@ -7,49 +7,76 @@ function resizeWindow(){
     window.resizeTo(WIDTH*scale, HEIGHT*scale)
     panels = document.getElementsByClassName("panel")
     for (let i = 0; i < panels.length; i++) {
-        panels[i].style.width = 310*scale+"px";
+        panels[i].style.width = 620*scale+"px";
         panels[i].style.height = 340*scale+"px";
+    }
+    card_spaces = document.getElementsByClassName("card_space")
+    for (let i = 0; i < card_spaces.length; i++) {
+        card_spaces[i].style.width = 60*scale+"px";
+        card_spaces[i].style.height = 80*scale+"px";
     }
     cards = document.getElementsByClassName("card")
     for (let i = 0; i < cards.length; i++) {
-        cards[i].style.width = 60*scale+"px";
-        cards[i].style.height = 80*scale+"px";
+        cards[i].style.width = 50*scale+"px";
+        cards[i].style.height = 70*scale+"px";
     }
 }
 
 window.addEventListener('resize', resizeWindow);
 
 // div要素を格納
-var cards = [];
+var card_spaces = [];
 
 window.onload = function(){
     // 数字格納 一時配列
     var arr = [];
 
-    var paneplayer1_panel = document.getElementById('player1_panel');
-    var paneplayer2_panel = document.getElementById('player2_panel');
+    var player1_panel = document.getElementById('player1_panel');
+    var player1_hand_panel = document.getElementById('player1_hand_panel');
+    var player1_monster_panel = document.getElementById('player1_monster_panel');
+    var player1_magic_panel = document.getElementById('player1_magic_panel');
+    var player2_panel = document.getElementById('player2_panel');
     
     // div要素作成
-    for (i = 0; i < 10; i++){
+    for (i = 0; i < 5; i++){
         var div = document.createElement('div');
-        div.className = 'card back';
+        div.id = 'player1_hand_panel_card_space_'+(i+1)
+        div.className = 'card_space';
         div.index = i;
         div.number = arr[i];
         div.innerHTML = '';
         div.onclick = select;
-        paneplayer1_panel.appendChild(div);
-        cards.push(div);
+        player1_hand_panel.appendChild(div);
+        card_spaces.push(div);
+
+        var card = document.createElement('div');
+        card.id = 'player1_hand_panel_card_'+(i+1)
+        card.className = 'card';
+        card.onclick = select;
+
+        setCard(div, card)
     }
-    for (i = 0; i < 10; i++){
+    for (i = 0; i < 5; i++){
         var div = document.createElement('div');
-        div.className = 'card back';
+        div.id = 'card_space_'+(i+1)
+        div.className = 'card_space';
         div.index = i;
         div.number = arr[i];
         div.innerHTML = '';
         div.onclick = select;
-        paneplayer2_panel.appendChild(div);
-        cards.push(div);
+        player1_monster_panel.appendChild(div);
+        card_spaces.push(div);
     }
+    // for (i = 0; i < 5; i++){
+    //     var div = document.createElement('div');
+    //     div.className = 'card_space';
+    //     div.index = i;
+    //     div.number = arr[i];
+    //     div.innerHTML = '';
+    //     div.onclick = select;
+    //     player1_magic_panel.appendChild(div);
+    //     card_spaces.push(div);
+    // }
     
     window.resizeTo(WIDTH*scale, HEIGHT*scale)
 }
@@ -59,5 +86,8 @@ function select(){
     
 }
 
+function setCard(space, card){
+    space.appendChild(card);
+}
 
 
